88a884129518d00f8df9240f8f5cf8c23bc7d637
ferbjmn <fernandezofc16@gmail.com>
2025-07-11 15:17:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -227,14 +227,14 @@ def main():
             st.header("📋 Resumen General")
             
             # Formatear columnas porcentuales
-            porcentajes = ["Dividend Yield %", "Payout Ratio", "ROA", "ROE", "Current Ratio", "Oper Margin", "Profit Margin", "WACC", "ROIC", "Creación de Valor (WACC vs ROIC)"]
+            porcentajes = ["Dividend Yield %", "Payout Ratio", "ROA", "ROE", "Current Ratio", "Oper Margin", "Profit Margin", "WACC", "ROIC", "Eva"]
             for col in porcentajes:
                 if col in df.columns:
                     df[col] = df[col].apply(lambda x: f"{x:.2%}" if pd.notnull(x) else "N/D")
             
             columnas_mostrar = [
                 "Ticker", "Nombre", "Sector", "Precio", "P/E", "P/B", "P/FCF", 
-                "Dividend Yield %", "Payout Ratio", "ROE", "ROA", "Current Ratio", "LTDebt/Eq", "Debt/Eq", "Profit Margin", "WACC", "ROIC", "Creación de Valor (WACC vs ROIC)"]
+                "Dividend Yield %", "Payout Ratio", "ROE", "ROA", "Current Ratio", "LTDebt/Eq", "Debt/Eq", "Profit Margin", "WACC", "ROIC", "Eva"]
             
             st.dataframe(
                 df[columnas_mostrar].dropna(how='all', axis=1),
